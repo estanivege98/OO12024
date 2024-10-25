5388e0f3fae7f9de023c38b43245fd3b0b01f740
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-10-25 09:28:07 -0300

    Falta implementar algunas cosas

---
+++ Practicas/Ejercicio18-PoliticasDeCancelacion/src/main/java/ar/edu/unlp/info/oo1/Reserva.java
@@ -0,0 +1,25 @@
+package ar.edu.unlp.info.oo1;
+
+public class Reserva {
+    private Usuario usuario;
+    private Propiedad propiedad;
+    private DateLapse periodo;
+
+    public Reserva(Usuario usuario, Propiedad propiedad, DateLapse periodo){
+        this.usuario = usuario;
+        this.propiedad = propiedad;
+        this.periodo = periodo;
+    }
+    public void removerReserva(){
+        getPropiedad().quitarPeriodoReserva(getPeriodo());
+    }
+    public Propiedad getPropiedad(){
+        return this.propiedad;
+    }
+    public DateLapse getPeriodo(){
+        return this.periodo;
+    }
+    public double calcularPrecioReserva(){
+        return periodo.sizeInDays() * propiedad.getPrecio();
+    }
+}
---
+++ Practicas/Ejercicio18-PoliticasDeCancelacion/src/main/java/ar/edu/unlp/info/oo1/Usuario.java
@@ -0,0 +1,52 @@
+package ar.edu.unlp.info.oo1;
+import java.time.LocalDate;
+import java.util.*;
+public class Usuario {
+    private String nombre;
+    private String direccion;
+    private String DNI;
+    private List<Reserva> reservas;
+    private List<Propiedad> propiedadesEnAlquiler;
+
+    public Usuario(String nombre, String direccion, String DNI){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.DNI = DNI;
+        this.reservas = new ArrayList<Reserva>();
+        this.propiedadesEnAlquiler = new ArrayList<Propiedad>();
+    }
+
+    public boolean crearReserva(DateLapse periodo, Propiedad propiedad){
+        if(propiedad.consultarDisponibilidad(periodo)){
+            Reserva reserva = new Reserva(this, propiedad, periodo);
+            this.reservas.add(reserva);
+            propiedad.getFechasReservadas().add(periodo);
+            return true;
+        }
+        return false;
+    }
+    public boolean cancelarReserva(Reserva reserva){
+        if(this.reservas.contains(reserva) && !reserva.getPeriodo().includesDate(LocalDate.now())){
+            this.reservas.remove(reserva);
+            //reserva.removerReserva();
+            reserva.getPropiedad().getFechasReservadas().remove(reserva.getPeriodo());
+            return true;
+        }
+        return false;
+    }
+    public List<Propiedad> getPropiedades(){
+        return this.propiedadesEnAlquiler;
+    }
+    public void agregarPropiedad(Propiedad propiedad){
+        this.propiedadesEnAlquiler.add(propiedad);
+    }
+    public void quitarPropiedad(Propiedad propiedad){
+        this.propiedadesEnAlquiler.remove(propiedad);
+    }
+
+    public double calcularIngresos(DateLapse periodo){
+        return this.propiedadesEnAlquiler.stream()
+                .mapToDouble(propiedad -> propiedad.consultarIngresosPeriodo(periodo))
+                .sum() * 0.75;
+    }
+}
