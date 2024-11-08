192dd9c4283ed0359c41bc49dd42dfe2c865fce9
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-11-08 09:31:50 -0300

    Solo estan los setUps

--- Practicas/Ejercicio20-LiquidacionDeHaberes/src/test/java/org/example/ContratoTest.java
+++ Practicas/Ejercicio20-LiquidacionDeHaberes/src/test/java/org/example/ContratoTest.java
@@ -1,4 +1,16 @@
 package org.example;
-
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 public class ContratoTest {
+    private PorHoras porHoras;
+    private DePlanta dePlanta;
+    private Empleado empleado;
+    
+    @BeforeEach
+    void setUp(){
+        empleado = new Empleado(0, true, "Juan", "Perez", 123456789, LocalDate.of(1980, 6, 12));
+        porHoras = new PorHoras(LocalDate.of(2021, 6, 1), 3000, 100, LocalDate.of(2026, 6, 1));
+        dePlanta = new DePlanta(LocalDate.of(2021, 6, 1), 30000, 1000, 1000);
+    }
 }
--- Practicas/Ejercicio20-LiquidacionDeHaberes/src/test/java/org/example/EmpleadoTest.java
+++ Practicas/Ejercicio20-LiquidacionDeHaberes/src/test/java/org/example/EmpleadoTest.java
@@ -1,4 +1,20 @@
 package org.example;
-
+import org.junit.jupiter.api.*;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
 public class EmpleadoTest {
+    private Empleado empleadoCasado;
+    private Empleado empleadoSoltero;
+    private Empleado empleadoCasadoConHijos;
+    private Empleado empleadoSolteroConHijos;
+    @BeforeEach
+    void setUp(){
+        empleadoCasado = new Empleado(0, true, "Juan", "Perez", 123456789, LocalDate.of(1980, 6, 12));
+        empleadoSoltero = new Empleado(0, false, "Edgardo", "Rodriguez", 123456789, LocalDate.of(1975, 6, 12));
+        empleadoCasadoConHijos = new Empleado(2, true, "Dan", "Gordo", 123456789, LocalDate.of(1990, 6, 12));
+        empleadoSolteroConHijos = new Empleado(2, false, "Miguel", "Abuelo", 123456789, LocalDate.of(1955, 6, 12));
+    }
+    
+    
+    
 }
