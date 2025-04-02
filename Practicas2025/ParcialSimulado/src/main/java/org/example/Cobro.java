86d8627756bc62235b070c04d6fa6abf020a3fb6
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-04-01 22:15:19 -0300

    Cambio de nombre

--- Practicas2025/ParcialSimulado/src/main/java/org/example/Cobro.java
+++ Practicas2025/ParcialSimulado/src/main/java/org/example/Cobro.java
@@ -1,13 +1,13 @@
 package org.example;
 import java.time.LocalDate;
-public abstract class Escritos {
+public abstract class Cobro {
 
     protected float monto;
     protected String originante;
     protected String destinatario;
     protected LocalDate fechaEmision;
 
-    public Escritos(String originante, String destinatario, float monto, LocalDate fechaEmision){
+    public Cobro(String originante, String destinatario, float monto, LocalDate fechaEmision){
         this.originante = originante;
         this.destinatario = destinatario;
         this.monto = monto;
@@ -15,5 +15,9 @@ public abstract class Escritos {
     }
 
     public abstract boolean estaVencido();
+
+    public float getMonto() {
+        return monto;
+    }
 
 }
